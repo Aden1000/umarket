e314069c619a46475feca0b609fd2c21f55187ed
Adriel <enehizenaadriel55@gmail.com>
2022-04-30 01:28:47 +0300

    Update.

--- Scripts/sanitize.php
+++
@@ -1,7 +0,0 @@
-<?php
-function sanitizeString($input){
-  $input=stripslashes($input);
-  $input=htmlentities($input);
-  return $input;
-}
-?>
--- Scripts/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_regenerate_id();
-setcookie('PHPSESSID',session_id(),time()+60,"/");
-echo "Done";
-?>
